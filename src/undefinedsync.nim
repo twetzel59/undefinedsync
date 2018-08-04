652f3faffda505ecd9819cbd0ec9f7f49f35feaf
twetzel59 <twetzel59@gmail.com>
2018-08-04 15:24:09 -0400

    Create basic multithreaded architecture * Added new worker thread * Added Channel to communicate with main thread * Created loading screen, and hid the main panel at load time * Implemented basic error handling

--- src/undefinedsync.nim
+++ src/undefinedsync.nim
@@ -1,22 +1,82 @@
-import os, webview
+import std / os
+import webview
 
-proc doDownload() = 
-    echo "Download"
+type
+  MessageKind = enum
+    mkExitSuccess,
+    mkExitFailure
+  
+  Message = object
+    case kind: MessageKind:
+    of mkExitFailure:
+      exceptionMsg: string
+    else:
+      discard
+
+proc initMessageExitSuccess(): Message =
+  Message(kind: mkExitSuccess)
+proc initMessageExitFailure(exceptionMsg: string): Message =
+  Message(kind: mkExitFailure, exceptionMsg: exceptionMsg)
+
+const inetError = (
+  title: "Network Error",
+  msg: "Sync thinks that you are not connected to the Internet. Check your network and proxy.")
+
+var wv: Webview
+var chan: Channel[Message]
+
+#proc checkInternet(): bool =
+#  false
+
+proc begin() {.thread, raises: [].} =
+  try:
+    #for i in 0..100000:
+    #  echo "hi"
+    #  if i == 1000:
+    #    raise newException(ValueError, "Test exception")
+    chan.send(initMessageExitSuccess())
+  except:
+    try:
+      chan.send(initMessageExitFailure(getCurrentExceptionMsg()))
+    except:
+      echo "Error sending exception message over channel. This is a bug!"
+
+proc doDownload() =
+  discard
 
 proc doUpload() =
-    echo "Upload"
+  discard
 
 proc main() =
-    let wv = newWebView("Sync", "file://" & getCurrentDir() & "/web/index.html", resizable = false)
+  try:
+    wv = newWebView("Sync", "file://" & getCurrentDir() & "/web/index.html", resizable = false)
 
     wv.bindProcs("api"):
-        proc download() = doDownload()
-        proc upload() = doUpload()
+      proc download() = doDownload()
+      proc upload() = doUpload()
 
-    try:
-        wv.run()
-    finally:
-        wv.exit()
+    chan.open()
+
+    var work: Thread[void]
+    createThread[void](work, begin)
+
+    wv.run()
+
+    joinThread(work)
+  finally:
+    let info = chan.tryRecv()
+    if info.dataAvailable:
+      case info.msg.kind:
+      of mkExitSuccess:
+        discard
+      of mkExitFailure:
+        let errBody = "The worker thread crashed! Exception message: " &
+          info.msg.exceptionMsg
+        echo errBody
+
+    chan.close()
+    wv.exit()
+    echo "Cleaning Up"
 
 when isMainModule:
-    main()
+  main()
